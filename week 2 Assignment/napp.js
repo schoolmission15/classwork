bc25dbed9880d6437d1d83e618449b9c3043346a
Jeffrey Emerson <jeffrey.leone@outlook.com>
2016-02-29 18:05:12 -0500

    redone

--- week 2 Assignment/napp.js
+++ week 2 Assignment/napp.js
@@ -2,11 +2,22 @@ var submit = document.getElementById("submit");
 submit.onclick = function() {
 	var name = document.getElementById("name").value;
 	var email = document.getElementById("email").value;
+    
 	var company = document.getElementById("company").value;
     var years = document.getElementById("years").value;
-	console.log(name, email, company, years);
+    var moves = document.getElementById("moves").value;
+    var fav = document.getElementById("fav").value;
+    var prior = document.getElementById("prior").value;
+    var fight = document.getElementById("fight").value;
+    var ideal = document.getElementById("ideal").value;
+    var win = document.getElementById("win").value;
+    var end = document.getElementById("end").value;
+    var level = document.getElementById("level").value;
+    var age = document.getElementById("age").value;
+	console.log(name, email, company, years, moves, fav, prior, fight, win, end, level, age);
 	
     var message = document.getElementById("message");
+    
 	message.innerHTML = " Hello " + name + " worked for " 
-	 + years + company + ", this is very impressive." ;
+	 + years + company + moves + ", this is very impressive." ;
 };
