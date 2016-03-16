d1733c793775fd2ee15a7ff799fad3dd6f7ecaeb
Jeffrey Valdivia <jeffrey.valdivia@f1006-06-34133.bmcc.cuny.edu>
2016-03-16 19:37:56 -0400

    UPDATE 3.5

---
+++ week7/seven.js
@@ -0,0 +1,42 @@
+var question = [
+    "is the sky blue?",
+    "what is the cpatial of latvial?"
+    "what is 2 + 3?"
+                ];
+
+var answers = [
+    "yes"
+    "riga"
+    "4"
+
+];
+
+var makeQuestion = function() {
+var q = document.createElement("p");
+q.innerHTML = "is the sky blue?";
+document.body.appendChild(q);
+
+var t = document.createElement("input");
+t.type = "text";
+t.placeholder = "answer";
+document.body.appendChild(t);
+
+var b = document.createElement("button");
+b.innerHTML = "submit answer.";
+b.onclick= function() {
+var m = document.createElement("p");
+var a = t.value;
+    if (a == "yes") {
+        m.innerHTML ="correct";
+    } else {
+        m.innerHTML = "no, you idiot!";
+}
+document.body.appendChild(m);
+
+}
+document.body.appendChild(b);};
+
+    for (var i = 0; i< 3; i++) {
+        //document.write(questions[i]);
+        //document.write("<br>");
+    }
